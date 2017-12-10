8f95f5d455fe512d967d883ab2b3ead29d7e326e
Sander Mertens <sander.mertens8@gmail.com>
2017-12-09 18:02:42 -0800

    New implementation for project.json

--- include/add.h
+++ include/add.h
@@ -1,16 +1,24 @@
+/* add.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
 
 #ifndef DRIVER_TOOL_ADD_H
 #define DRIVER_TOOL_ADD_H
 
-#ifdef __cplusplus
-extern "C" {
+#include <corto/corto.h>
+#include <driver/tool/add/_project.h>
+#include <corto/c/c.h>
+#include <parson/parson.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
 #endif
-
-char* corto_tool_lookupPackage(char* str);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* DRIVER_TOOL_ADD_H */
 
--- src/add.c
+++ src/add.c
@@ -1,145 +1,122 @@
-/* Copyright (c) 2010-2017 the corto developers
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 
 #include <driver/tool/add/add.h>
-#include <corto/argparse/argparse.h>
 
-corto_string corto_tool_lookupPackage(corto_string str) {
-    corto_string package = NULL;
-
-    corto_object p = corto_resolve(NULL, str);
-
-    if (!p) {
-        if (!corto_locate(str, NULL, CORTO_LOCATION_LIB)) {
-            corto_seterr("package '%s' not found", str);
-            goto error;
-        } else {
-            package = str;
-        }
-    } else {
-        if (!corto_instanceof(corto_package_o, p)) {
-            corto_seterr("object '%s' is not a package", str);
-            goto error;
-        } else {
-            package = corto_fullpath(NULL, p);
-        }
-        corto_release(p);
-    }
-
-    return package;
-error:
-    return NULL;
-}
+/* This command uses parson instead of corto serialization/deserialization to
+ * ensure that any members not part of the corto package type are preserved. */
 
 int cortomain(int argc, char *argv[]) {
-    corto_ll silent, mute, nobuild, nocoverage, project, packages;
-    corto_bool build = FALSE;
-    CORTO_UNUSED(argc);
+    char *pkg = NULL;
+    JSON_Value *json = NULL;
 
-    corto_argdata *data = corto_argparse(
-      argv,
-      (corto_argdata[]){
-        /* Ignore first argument */
-        {"$0", NULL, NULL},
-        {"--silent", &silent, NULL},
-        {"--mute", &mute, NULL},
-        {"--nobuild", &nobuild, NULL},
-        {"--nocoverage", &nocoverage, NULL},
-
-        /* Match at most one project directory */
-        {"$?*", &project, NULL},
-
-        /* At least one package must be specified */
-        {"$+*", &packages, NULL},
-        {NULL}
-      }
-    );
-
-    if (!data) {
+    if (argc == 2) {
+        pkg = argv[1];
+    } else if (argc == 3) {
+        if (corto_chdir(argv[1])) {
+            corto_throw(NULL);
+            goto error;
+        }
+        pkg = argv[2];
+    } else {
+        corto_throw("invalid number of arguments");
         goto error;
     }
 
-    /* Move to project directory */
-    if (project) {
-        if (corto_chdir(corto_ll_get(project, 0))) {
+    if (pkg[0] == '/') {
+        pkg ++;
+    }
+
+    if (!strlen(pkg)) {
+        corto_throw("invalid package identifier");
+        goto error;
+    }
+
+    if (!corto_file_test("project.json")) {
+        corto_throw("directory '%s' does not contain a corto project",
+            corto_cwd());
+        goto error;
+    }
+
+    json = json_parse_file("project.json");
+    if (!json) {
+        corto_throw("failed to parse 'project.json'");
+        goto error;
+    }
+
+    JSON_Object *jsonObject = json_value_get_object(json);
+    if (!jsonObject) {
+        corto_throw("invalid JSON in 'project.json': expected object");
+        goto error;
+    }
+
+    JSON_Object *json_m_value = NULL;
+    if (json_object_has_value(jsonObject, "value")) {
+        json_m_value = json_object_get_object(jsonObject, "value");
+        if (!json_m_value) {
+            /* If JSON has a value member, but wasn't resolved by the previous
+             * call, it is not of an object type */
+            corto_throw("invalid JSON: expected 'value' to be a JSON object");
             goto error;
         }
+    } else {
+        /* This is not an error, a project file can leave all project
+         * settings to defaults. Add a value member. */
+        json_object_set_value(
+            jsonObject, "value", json_value_init_object());
+        json_m_value = json_object_get_object(jsonObject, "value");
     }
 
-    /* Load packages */
-    if (packages) {
-        corto_iter iter = corto_ll_iter(packages);
-        while (corto_iter_hasNext(&iter)) {
-            /* Test whether package exists */
-            corto_string package =
-                corto_tool_lookupPackage(corto_iter_next(&iter));
-            if (!package) {
-                goto error;
-            }
+    JSON_Array *json_m_use = NULL;
+    if (json_object_has_value(json_m_value, "use")) {
+        json_m_use = json_object_get_array(json_m_value, "use");
+        if (!json_m_use) {
+            /* If JSON has a use member, but wasn't resolved by the previous
+             * call, it is not of an array type */
+            corto_throw("invalid JSON: expected 'value.use' to be a JSON array");
+            goto error;
+        }
+    }  else {
+        json_object_set_value(
+            json_m_value, "use", json_value_init_array());
+        json_m_use = json_object_get_array(json_m_value, "use");
+    }
 
-            /* Use fully scoped name from here */
-            if (!corto_loadRequiresPackage(package)) {
-                corto_mkdir(".corto");
-                corto_file f = corto_fileAppend(".corto/packages.txt");
-                if (!f) {
-                    corto_seterr("failed to open .corto/packages.txt (check permissions)");
-                    goto error;
-                }
+    /* Add package if it wasn't already added */
+    int i;
+    for (i = 0; i < json_array_get_count(json_m_use); i ++) {
+        const char *use = json_array_get_string(json_m_use, i);
+        if (use[0] == '/') {
+            use ++;
+        }
 
-                /* Only add fully scoped names to package file */
-                if (*package != '/') {
-                    fprintf(corto_fileGet(f), "/%s\n", package);
-                } else {
-                    fprintf(corto_fileGet(f), "%s\n", package);
-                }
-                corto_fileClose(f);
-                build = TRUE;
-
-                if (!silent) {
-                    printf("package '%s' added to project\n", package);
-                }
-            } else {
-                if (!silent) {
-                    printf("package '%s' is already added to the project\n", package);
-                }
-            }
-
+        if (!strcmp(use, pkg)) {
+            /* Package is already added */
+            break;
         }
     }
 
-    if (build && !nobuild) {
-        corto_load("driver/tool/build", 3, (char*[]){
-          "build",
-          silent ? "--silent" : "",
-          mute ? "--mute" : "",
-          nocoverage ? "--nocoverage" : "",
-          NULL
-        });
+    /* If loop finished, we didn't find the package */
+    if (i == json_array_get_count(json_m_use)) {
+        json_array_append_string(json_m_use, pkg);
     }
 
-    corto_argclean(data);
+    char *escapedStr = json_serialize_to_string_pretty(json);
+
+    /* Parson escapes '/' which doesn't look nice in the project.json file.
+     * Until this becomes an optional feature in parson, replace \/ with / */
+    char *str = strreplace(escapedStr, "\\/", "/");
+    json_value_free(json);
+    free(escapedStr);
+
+    /* Write to file */
+    FILE *f = fopen("project.json", "w");
+    fprintf(f, "%s\n", str);
+    fclose(f);
+    free(str);
 
     return 0;
 error:
-    corto_error("add: %s", corto_lasterr());
+    if (json) {
+        json_value_free(json);
+    }
     return -1;
 }
-
