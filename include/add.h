3ad92b60dafc9d9afa254b4264f123eeb2ca167e
Sander Mertens <sander.mertens8@gmail.com>
2018-06-21 23:10:40 -0700

    Remove redundant file, includes

--- include/add.h
+++ include/add.h
@@ -8,17 +8,7 @@
 #define DRIVER_TOOL_ADD_H
 
 #include <corto/corto.h>
-#include <driver/tool/add/_project.h>
 #include <corto/c/c.h>
 #include <parson/parson.h>
 
-/* $header() */
-/* Definitions that are required by package headers (native types) go here. */
-/* $end */
-
-/* $body() */
-/* Definitions here that need your package headers go here. */
-/* $end */
-
 #endif
-
